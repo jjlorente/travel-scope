0caf3f088b9ba0756d8fc267ed7a546150617929
JJLorente <joel.jimenez.lorente@gmail.com>
2023-04-14 19:10:43 +0200

    Footer add

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 import './footer.css'
 import video2 from '../../Assets/video2.mp4'
 import { FiSend } from 'react-icons/fi'
-
+import { MdOutlineTravelExplore } from 'react-icons/md'
+import { AiOutlineTwitter } from 'react-icons/ai'
+import { AiFillYoutube } from 'react-icons/ai'
+import { AiFillInstagram } from 'react-icons/ai'
+import { FaTripadvisor } from 'react-icons/fa'
+import { FiChevronRight } from 'react-icons/fi'
 
 const Footer = () => {
   return (
@@ -23,6 +28,132 @@ const Footer = () => {
             <button className='btn flex' type='submit'>
               SEND <FiSend className="icon"/>
             </button>
+          </div>
+        </div>
+
+        <div className="footerCard flex">
+          <div className="footerIntro flex">
+            <div className="logoDiv">
+              <a href="" className='logo flex'>
+                <MdOutlineTravelExplore className="icon"/>  Travel.
+              </a>
+            </div>
+
+            <div className="footerParagraph">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam inventore eos fguga hic cum, tempore non odio, sed cupiditate ipsumin, atque soluta?
+            </div>
+
+            <div className="footerSocials flex">
+              <AiOutlineTwitter className="icon"/>
+              <AiFillYoutube className="icon"/>
+              <AiFillInstagram className="icon"/>
+              <FaTripadvisor className="icon"/>
+            </div>
+          </div>
+
+          <div className="footerLinks grid">
+
+            {/*GROUP1*/}
+            <div className="linkGroup">
+              <span className="groupTitle">
+                OUR AGENCY
+              </span>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon"/>
+                Services
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon"/>
+                Insurance
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon"/>
+                Agency
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon"/>
+                Tourism
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon"/>
+                Payment
+              </li>
+            </div>
+
+            {/*GROUP2*/}
+            <div className="linkGroup">
+              <span className="groupTitle">
+                PARTNERS
+              </span>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon"/>
+                Bookings
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon"/>
+                Rentcars
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon"/>
+                HostelWorld
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon"/>
+                Trivago
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon"/>
+                TripAdvisor
+              </li>
+            </div>
+
+            {/*GROUP3*/}
+            <div className="linkGroup">
+              <span className="groupTitle">
+                LAST MINUTE
+              </span>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon"/>
+                London
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon"/>
+                California
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon"/>
+                Indonesia
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon"/>
+                Europe
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon"/>
+                Oceania
+              </li>
+            </div>
+
+          </div>
+
+          <div className="footerDiv flex">
+            <small>BEST TRAVEL WEBSITE THEME</small>
+            <small>COPYRIGHTS RESERVED - JOEL JIMÉNEZ</small>
           </div>
         </div>
       </div>
