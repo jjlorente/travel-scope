230cbe1e6f6af21256f2d2f9e7d4c9746d07e896
JJLorente <joel.jimenez.lorente@gmail.com>
2023-04-14 18:25:55 +0200

    Main section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react'
 import './app.css'
+import Footer from './Components/Footer/Footer'
+import Home from './Components/Home/Home'
+import Navbar from './Components/Navbar/Navbar'
+import Main from './Components/Main/Main'
 
 function App() {
   return (
-    <h2>App</h2>
+    <>
+    <Navbar />
+    <Home />
+    <Main />
+    <Footer />
+    </>
   )
 }
 
---
+++ src/Components/Main/Main.jsx
@@ -0,0 +1,136 @@
+import React from 'react'
+import './main.css'
+import img from '../../Assets/image1.jpg'
+import img2 from '../../Assets/image2.jpg'
+import {HiOutlineLocationMarker} from 'react-icons/hi'
+import {BsClipboardCheck} from 'react-icons/bs'
+
+const Data = [
+  {
+    id:1,
+    imgSrc: img,
+    destTitle:'Bora Bora',
+    location: 'New Zealand',
+    grade: 'CULTURAL RELAX',
+    fees: '700$',
+    description: 'The epitome of romance, Bora Bora is one of the best travel destinatios in the World. This place is known for its luxurious stays and adventurous activities.'
+  },
+
+  {
+    id:2,
+    imgSrc: img2,
+    destTitle:'Machu Picchu',
+    location: 'Peru',
+    grade: 'CULTURAL RELAX',
+    fees: '600$',
+    description: 'The epitome of romance, Bora Bora is one of the best travel destinatios in the World. This place is known for its luxurious stays and adventurous activities.'
+  },
+  {
+    id:3,
+    imgSrc: img,
+    destTitle:'Great Barrier Reef',
+    location: 'Autralia',
+    grade: 'CULTURAL RELAX',
+    fees: '800$',
+    description: 'The epitome of romance, Bora Bora is one of the best travel destinatios in the World. This place is known for its luxurious stays and adventurous activities.'
+  },
+  {
+    id:4,
+    imgSrc: img2,
+    destTitle:'Cappadocia',
+    location: 'Turkey',
+    grade: 'CULTURAL RELAX',
+    fees: '400$',
+    description: 'The epitome of romance, Bora Bora is one of the best travel destinatios in the World. This place is known for its luxurious stays and adventurous activities.'
+  },
+  {
+    id:5,
+    imgSrc: img,
+    destTitle:'Mexico',
+    location: 'Mexico',
+    grade: 'CULTURAL RELAX',
+    fees: '1100$',
+    description: 'The epitome of romance, Bora Bora is one of the best travel destinatios in the World. This place is known for its luxurious stays and adventurous activities.'
+  },
+  {
+    id:6,
+    imgSrc: img2,
+    destTitle:'Cinque Terre',
+    location: 'Italy',
+    grade: 'CULTURAL RELAX',
+    fees: '850$',
+    description: 'The epitome of romance, Bora Bora is one of the best travel destinatios in the World. This place is known for its luxurious stays and adventurous activities.'
+  },
+  {
+    id:7,
+    imgSrc: img,
+    destTitle:'Taj Mahal',
+    location: 'India',
+    grade: 'CULTURAL RELAX',
+    fees: '900$',
+    description: 'The epitome of romance, Bora Bora is one of the best travel destinatios in the World. This place is known for its luxurious stays and adventurous activities.'
+  },
+  {
+    id:8,
+    imgSrc: img2,
+    destTitle:'Angkor Wat',
+    location: 'Cambodia',
+    grade: 'CULTURAL RELAX',
+    fees: '740$',
+    description: 'The epitome of romance, Bora Bora is one of the best travel destinatios in the World. This place is known for its luxurious stays and adventurous activities.'
+  },
+]
+
+const Main = () => {
+  return (
+    <section className='main container section'>
+      <div className="secTitle">
+        <h3 className="title">
+          Most visited destinations
+        </h3>
+      </div>
+
+      <div className="secContent grid">
+        {
+          Data.map(({id, imgSrc, destTitle, location, grade, fees, description}) => {
+            return (
+              <div key={id} className="singleDestination">
+                <div className="imageDiv">
+                  <img src={imgSrc} alt={destTitle} />
+                </div>
+
+                <div className="cardInfo">
+                  <h4 className="destTitle">{destTitle}</h4>
+                  <span className="continent flex">
+                    <HiOutlineLocationMarker className='icon'/>
+                    <span className="name">{location}</span>
+                  </span>
+
+                  <div className="fees flex">
+                    <div className="grade">
+                      <span>{grade}<small>+1</small></span>
+                    </div>
+
+                    <div className="price">
+                      <h5>{fees}</h5>
+                    </div>
+                  </div>
+
+                  <div className="desc">
+                    <p>{description}</p>
+                  </div>
+
+                  <button className='btn flex'>
+                    DETAILS <BsClipboardCheck className='icon'/>
+                  </button>
+                </div>
+              </div>
+            )
+          })
+        }
+      </div>
+    </section>
+  )
+}
+
+export default Main
