29adcd6b222b725fbc453a80c24a7bec1659d854
JJLorente <joel.jimenez.lorente@gmail.com>
2023-05-12 19:21:38 +0200

    manifest fixed

--- src/App.js
+++ src/App.js
@@ -7,12 +7,12 @@ import Main from './Components/Main/Main'
 
 function App() {
   return (
-    <>
-    <Navbar />
-    <Home />
-    <Main />
-    <Footer />
-    </>
+    <div>
+      <Navbar />
+      <Home />
+      <Main />
+      <Footer />
+    </div>
   )
 }
 
